f9b08cf6386594060ab82aed7730d87cce88ae0e
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2025-06-15 00:27:13 +0500

    create user register api

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -1,6 +1,36 @@
+import {User} from '../models/user.js';
+import bycrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 export const register = async (req, res) => {
     try{
-        res.send("Register API")
+        const { email, name, password } = req.body();
+
+        let user = await User.findOne({ email });
+
+        if(user) 
+            return res.status(400).json({
+                message: "User Already exists",
+            });
+
+            const hashPassword = await bycrypt.hash(password, 10);
+
+            user = {
+                name, 
+                email,
+                password: hashPassword,
+            };
+
+            const otp = Math.floor(Math.random() * 1000000 );
+
+            const activationToken = jwt.sign({
+                user, 
+                otp,                
+            }, process.env.Activation_Secret,
+        {
+            expiresIn: "5m",
+        }
+    )   
     }
     catch(error){
         res.status(500).json({
@@ -8,3 +38,5 @@ export const register = async (req, res) => {
         });
     }
 }
+
+export const loginUser = TryCatch()
