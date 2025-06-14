bbde1198118901d37649647eacb79bbb832caf7f
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2025-06-14 23:11:24 +0500

    user schema created

---
+++ Server/controllers/user.js
@@ -0,0 +1,10 @@
+export const register = async (req, res) => {
+    try{
+        res.send("Register API")
+    }
+    catch(error){
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+}
--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./database/db.js";
 
 dotenv.config();
 
@@ -11,6 +12,13 @@ app.get('/', (req, res) => {
     res.send('hello world');
 })
 
+// importing routes 
+import userRoutes from "./routes/user.js";
+
+// using routes
+app.use('/api', userRoutes);
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+    connectDB();
 })
