d1fc6baee6a59212cb3b13d17f061e6af03ad639
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2025-06-29 02:13:05 +0500

    frontend started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Pages/Home/Home';
+import Header from './Components/Header/Header';
+
+
 function App() {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element= {<Home/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header>
+        <div className='logo'> E-Learning </div>
+        <div className='link'>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/courses'}>Courses</Link>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/account'}>Account</Link>
+        </div>
+        
+    </header>
+    
+  )
+}
+
+export default Header
