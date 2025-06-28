5dd990c39c691ae322cf9524ca9e593b2b19c7b6
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2025-06-29 03:48:45 +0500

    frontend continue...

--- frontend/src/Pages/Auth/Register.jsx
+++ frontend/src/Pages/Auth/Register.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './Auth.css';
+import { Link } from 'react-router-dom';
+
 
 function Register() {
   return (
-    <div>Register</div>
+     <div className="auth-page">
+        <div className="auth-form">
+            <h2>Register</h2>
+            <form >
+                <label htmlFor="name">Name</label>
+                <input type="text" required />
+
+                <label htmlFor="email">Email</label>
+                <input type="email" required />
+
+                <label htmlFor="password">Password</label>
+                <input type="password" required />
+
+                <div className="common-btn"> Register </div>
+            </form>
+            <p>
+                Already have an Account? <Link to='/login'> Login </Link>
+            </p>
+        </div>
+    </div>
   )
 }
 
--- frontend/src/Pages/Auth/Verify.jsx
+++ frontend/src/Pages/Auth/Verify.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Auth.css';
+import { Link } from 'react-router-dom';
 
 function Verify() {
   return (
-    <div>Verify</div>
+    <div className="auth-page">
+        <div className="auth-form">
+            <h2>Verify Account</h2>
+            <form >
+                <label htmlFor="otp">OTP</label>
+                <input type="number" required />
+
+                <div className="common-btn"> Verify </div>
+            </form>
+            <p>
+                Go to <Link to='/login'> Login </Link> Page 
+            </p>
+        </div>
+    </div>
   )
 }
 
