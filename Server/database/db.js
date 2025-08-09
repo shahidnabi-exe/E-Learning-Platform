d42857cea730be94b6ff2355cfe1588173ee54bc
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2025-08-09 16:12:47 +0500

    frontend continue...

--- Server/database/db.js
+++ Server/database/db.js
@@ -1,8 +1,8 @@
 import mongoose  from "mongoose";
-
+const url="mongodb://localhost:27017/ELearningPlatform"
 export const connectDB = async () => {
     try{
-        const {connection} = await mongoose.connect(process.env.DB);
+        const {connection} = await mongoose.connect(url);
         console.log(`Connected to MongoDB: ${connection.name}`);
 
     }
